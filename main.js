2dc0f5f5641e9cc608651ec1e2991f33bbb9fb36
Donut5896 <Nutchanokchon5@gmail.com>
2021-02-20 23:09:33 +0700

    20/02/21

--- main.js
+++ main.js
@@ -1,15 +1,89 @@
 const inputBar = document.getElementById('inputbar');
 const addBtn = document.querySelector(".plus-btn");
-const listContainer = document.querySelector('.list-container');
 const lists = document.getElementById('lists');
 
 
-function main(){
-addBtn.addEventListener('click', (e) => {
-    let inputList = inputBar.value;
+
+//delete and check functionality
+function deleteItem(e){
+    const item = e.target;
+     console.log(item);
+    //delete items
+    if(item.classList[0] === "delete-btn"){
+        const todo = item.parentElement.parentElement;
+        todo.classList.add("fall");
+        todo.addEventListener('transitionend', function() {
+            todo.remove;
+        })
+        console.log(item.classList[0]);
+    }
+    //checked items
+    if(item.classList[0] === "check-btn"){
+        const todo = item.parentElement.parentElement;
+        todo.classList.toggle("completedItem")
+          console.log(todo);
+    }
+}
+
+
+
+// create lists with delete and check buttons
+function addTodo(event){
+    event.preventDefault();
+    // todo li
+    const todoLi = document.createElement('li');
+    todoLi.innerHTML = `<p>${inputBar.value}</p>`;
+    todoLi.classList.add('list');
+    lists.appendChild(todoLi);
+    //if input equals to null
+    if(inputBar.value === ""){
+        return null;
+    }
+    // button div
+    const buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+    
+
+    //check button
+    const checkedButton = document.createElement('button');
+    checkedButton.innerHTML = ` <i class="fa fa-check"></i>`;
+    checkedButton.classList.add('check-btn');
+    buttons.appendChild(checkedButton)
+    
+    //delete button
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = ` <i class="fa fa-trash"></i>`;
+    deleteButton.classList.add('delete-btn');
+    buttons.appendChild(deleteButton); 
+    todoLi.appendChild(buttons);
+
+    //clear todo input bar
+    inputBar.value = "";
+}
+
+
+
+
+
+//event listeners
+addBtn.addEventListener('click', addTodo);
+lists.addEventListener('click', deleteItem);
+
+
+
+
+
+
+/*function createListItem(event){
+    //let inputList = inputBar.value;
+
     let list = document.createElement('li');
+    list.classList.add("list")
+    const attr = document.createAttribute('data-id');
+    attr.value = id;
+    list.setAttributeNode(attr);
     list.innerHTML = `
-                        <p>${inputList}</p>
+                        <p>${value}</p>
                         <div class="buttons">
                             <button id="check-btn" class="check-btn">
                                 <i class="fa fa-check"></i>
@@ -19,22 +93,19 @@ addBtn.addEventListener('click', (e) => {
                             </button>
                         </div>   
                     `;
-    list.classList.add("list")
-    list.id = 'list';
-    lists.appendChild(list);
 
-
-      
-   
+    
    
 
-let checkBtn = document.querySelectorAll('.check-btn');
-let deleteBtn = document.querySelectorAll('.delete-btn');
-  
+const checkBtn = list.querySelector('.check-btn');
+const deleteBtn = list.querySelector('.delete-btn'); 
+deleteBtn.addEventListener('click', deleteItem);
 
- 
+ lists.appendChild(list); 
+ console.log(list)
+}
 
-    function dount(){
+   /* function dount(){
        let list = document.querySelectorAll('.list');
         list.forEach(function(e) {
         e.addEventListener('click', () => {
@@ -44,54 +115,28 @@ let deleteBtn = document.querySelectorAll('.delete-btn');
   
     }
   
- checkBtn.forEach(dount);
-  deleteBtn.forEach(waffle);
-
-    function waffle(){
-        let list = document.querySelectorAll('.list');
-        list.forEach(function(i) {
-        i.addEventListener('click', () => {
-             i.style.display = 'none';      
-        })  
-    })    
-    }
-
-  
-  
-  
-  });
+ checkBtn.forEach(dount);*
     
   
 
 
 }
 
-  
-    
- //check button
-function check(e){
+ //check button 
+/*function check(e){
         let list = document.querySelectorAll('.list');
         list.forEach(function(e) {
         e.addEventListener('click', () => {
-             e.style.textDecoration = 'line-through'; 
-             console.log(list);
+        e.style.textDecoration = 'line-through'; 
+        console.log(list);
         })
-       
     })
-}
-  
+} */
+
+
 //delete button
-function deleted(){
-    let list = document.querySelectorAll('.list');
-    list.forEach(function(i){
-        i.addEventListener('click', () => {
-           
-        })
-    })
-}
 
 
 
 
 
-main();
