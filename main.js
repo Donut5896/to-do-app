5230b25872226fc1c9c381257ecfcd2f971247ef
Donut5896 <Nutchanokchon5@gmail.com>
2021-02-25 22:52:53 +0700

    commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const inputBar = document.getElementById('inputbar');
 const addBtn = document.querySelector(".plus-btn");
 const lists = document.getElementById('lists');
-const filter = document.getElementById('categories');
+const filter = document.querySelector('.categories');
 
 
 
@@ -28,16 +28,14 @@ function createListItem( toDo, id, done, trash){
     const item = `<li class="list">
                         <p class="text ${LINE}">${toDo}</p>
                         <div class="buttons">
-                                <i class="fa ${DONE} co" id="${id}" job="complete"></i>
-                                <i class="fa fa-trash" id="${id}" job="delete"></i>
+                                <i class="fa ${DONE} co" id="${id}" job="complete" value="completed"></i>
+                                <i class="fa fa-trash" id="${id}" job="delete" value="uncompleted"></i>
                         </div> 
                     </li>  
                     `;
 
     const position = "beforeend";
     lists.insertAdjacentHTML(position, item);
-   
-
 }
 
 
@@ -88,27 +86,96 @@ lists.addEventListener("click", function(event){
          checkbutton(element);
     }else if(itemJob == "delete"){
         deletebutton(element);
-        console.log(element);
     }
-    filter.addEventListener('click', filterItem(element));
+  
 })
 
+ //filtering function
+ 
+/*let item = document.querySelectorAll('option');
+let ul = document.querySelector('select')
+item.forEach(el => {
+    el.addEventListener('click', function(){
+        ul.querySelector('.active').classList.remove('active');
+        el.classList.add('active');
+    })
+})*/
 
 
-//filtering function
+
+
+
+
+ /*filter.addEventListener("click", function(element){
+        
+    
+    
+
+    item.
+
+
+     if(element.target.option = "item"){
+        //remove and add active class
+      
+        filter.contains("active").remove("active")
+        element.target.classList.add("active"); 
+      
+     
+        
+     }
+   const list = document.querySelectorAll('.list');
+
+    if(element.target.classList.contains("item")){
+        console.log(element);
+        //remove and add active class
+        filter.querySelector(".active").classList.remove("active");
+        element.target.classList.add("active");
+        console.log(element);
+        //get data-name of selected items and store in filtername
+        let filterName = element.target.getAttribute("data-name");
+        list.forEach((li) => {
+
+            //get data name value
+            let filterList = li.getAttribute("data-name");
+            console.log(filterList);
+
+            if( (filterList == filterName) || (filterName == "all")){
+                li.classList.add("show");
+                li.classList.remove("hide");
+            }else{
+                li.classList.add("hide");
+                li.classList.remove("show");
+            }
+        })
+    }*/
+
+
+
+
+
+
+
+
+
+filter.addEventListener('click', filterItem);
+
+
+
+
+
 function filterItem(event){
-    const items = lists.childNodes;
+   // const items = document.querySelectorAll(".list");
    // const itemsJob = event.target.attributes.job.value;
 
-    for(let i=1; i < items.length; i++){
-        switch(event.target){
-            case "All":
+    for(let i=1; i < event.length; i++){
+        switch(event.target.value){
+            case "all":
                 items[i].style.display = "flex";
                 break;
             case "completed":
-            if(items[i].classList.contains('CHECK')){
+            if(items[i].classList.contains('fa-check-circle')){
                 items[i].style.display = "flex";
-            }else{
+            }else {
                 items[i].style.display = "none";
             }
             break;
@@ -121,5 +188,5 @@ function filterItem(event){
             break;
         }
     }
-}
+} 
 
